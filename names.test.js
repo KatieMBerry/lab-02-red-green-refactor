8d5e4973f52e2214866d5cc85eedffec1a802b7d
Katie <katieberrym@gmail.com>
2020-12-01 13:35:21 -0800

    added test file and names.js - test-1  pass

---
+++ names.test.js
@@ -0,0 +1,10 @@
+const { getNames } = require('./names');
+
+describe('getNames function', () => {
+    it('returns the name property of an object', () => {
+        const name = getNames();
+
+    })
+
+
+});
