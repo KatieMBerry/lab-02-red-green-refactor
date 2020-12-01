285d2c9490e6a64bd47f00f8c7878f7e782b469e
Katie <katieberrym@gmail.com>
2020-12-01 15:50:58 -0800

    capitalizeAndFilter test-1 pass

---
+++ capitalizeandfilter.js
@@ -0,0 +1,13 @@
+const capitalizeAndFilter = () => {
+
+};
+
+
+module.exports = {
+    capitalizeAndFilter
+};
+
+/*Write a test
+create a function, use a for loop to get test to pass
+green, commit
+refactor, use array methods to instead of a for loop*/
---
+++ capitalizeandfilter.test.js
@@ -0,0 +1,15 @@
+const { capitalizeAndFilter } = require('./capitalizeandfilter');
+
+describe('capitalizeAndFilter function', () => {
+
+    it('takes an array of strings, capitalize all strings, and filters out any string that starts with the letter f', () => {
+        const arrayOfStrings = ['spot', 'rover', 'bingo', 'fred'];
+
+
+    })
+
+    // it('checks the original array stays the same', () => {
+
+    //     expect(numbers).toEqual([1, 2, 3]);
+    // })
+});
