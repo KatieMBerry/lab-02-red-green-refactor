2fa5d98fdfe9ad6fbce260da15ee81c40a4eb4aa
Katie <katieberrym@gmail.com>
2020-12-01 15:43:46 -0800

    copyandpush spread test pass

--- copyandpush.js
+++ copyandpush.js
@@ -1,6 +1,6 @@
 const copyAndPush = (array, num) => {
-    array.push(num);
-    return array;
+    const newArray = [...array, num];
+    return newArray;
 };
 
 
--- copyandpush.test.js
+++ copyandpush.test.js
@@ -1,17 +1,16 @@
 const { copyAndPush } = require('./copyandpush');
 
 describe('copyAndPush function', () => {
+    const numbers = [1, 2, 3];
+
     it('returns a new array with all the items in the original array and a new item pushed to the end', () => {
-        const numbers = [1, 2, 3];
         const newArray = copyAndPush(numbers, 4);
 
         expect(newArray).toEqual([1, 2, 3, 4]);
     })
 
-    // it('returns the name property of an object', () => {
-    //     const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
-    //     const name = getName(character);
+    it('checks the original array stays the same', () => {
 
-    //     expect(name).toEqual('Aang');
-    // })
+        expect(numbers).toEqual([1, 2, 3]);
+    })
 });
