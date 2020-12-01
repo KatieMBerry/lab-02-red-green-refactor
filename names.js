23b569721a142dc51c2e21a6c38670474328fada
Katie <katieberrym@gmail.com>
2020-12-01 14:12:07 -0800

    bracket test pass

--- names.js
+++ names.js
@@ -1,8 +1,27 @@
-const getNames = () => {
 
 
+const getName = (spot) => {
+
+    const name = spot['name'];
+
+    return name;
 }
 
+
+
+
 module.exports = {
-    getNames
+    getName
 };
+
+
+
+/* Write a test
+create your function, use bracket notation to get the objects name property
+pass tests and commit
+refactor, use dot notation to get the objects name
+tests stay green, commit
+refactor, destructure to get the objects name
+tests stay green, commit
+refactor, destructure function parameters and return name
+tests stay green, commit */
--- names.test.js
+++ names.test.js
@@ -1,10 +1,19 @@
-const { getNames } = require('./names');
+const { getName } = require('./names');
 
-describe('getNames function', () => {
+describe('getName function', () => {
     it('returns the name property of an object', () => {
-        const name = getNames();
+        const spot = { name: 'spot', age: 5, weight: '20 lbs' }
+        // const { name, age, weight } = spot;
 
+        const name = getName(spot);
+
+        expect(name).toEqual('spot');
     })
 
+    // it('returns the name property of an object', () => {
+    //     const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
+    //     const name = getName(character);
 
+    //     expect(name).toEqual('Aang');
+    // })
 });
