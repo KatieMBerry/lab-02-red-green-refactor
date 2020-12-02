6eb85bac5be19c0cc9a0bf96ebbb86187295b66d
Katie <katieberrym@gmail.com>
2020-12-01 19:55:15 -0800

    fetchQuotes test-3 refactor pass

--- fetch-quotes.js
+++ fetch-quotes.js
@@ -1,9 +1,18 @@
 const request = require('superagent');
 
 const fetchQuotes = async () => {
-    const response = await request.get('http://futuramaapi.herokuapp.com/api/quotes');
+    const { body } = await request.get('http://futuramaapi.herokuapp.com/api/quotes/1');
 
-    return response.body[0];
+    const result = body[0];
+
+    const quote =
+    {
+        character: result.character,
+        quote: result.quote,
+        image: result.image
+    }
+
+    return quote;
 };
 
 
